b5ea22def206b734555d664729fb4660ea49638e
fscontrol <127598273+fscontrol@users.noreply.github.com>
2025-03-17 11:55:21 +0300

    F

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cwtlib",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         'numpy>=1.18.1',
